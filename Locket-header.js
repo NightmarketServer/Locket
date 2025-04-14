466c6101b4a19639bf0c83538c8781438bd7c974
Nightmarket <162085003+NightmarketServer@users.noreply.github.com>
2025-04-14 22:20:23 +0700

    Add files via upload

---
+++ Locket-header.js
@@ -0,0 +1,27 @@
+const version = 'V1.0.3';
+
+function setHeaderValue(headers, key, value) {
+    const lowerKey = key.toLowerCase();
+    if (lowerKey in headers) {
+        headers[lowerKey] = value;
+    } else {
+        headers[key] = value;
+    }
+}
+
+var modifiedHeaders = $request.headers;
+
+const headersToModify = {
+    "X-RevenueCat-ETag": "",
+    "If-None-Match": "",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache"
+};
+
+for (let key in headersToModify) {
+    setHeaderValue(modifiedHeaders, key, headersToModify[key]);
+}
+
+console.log("🛠 Modified Headers:", JSON.stringify(modifiedHeaders, null, 2));
+
+$done({ headers: modifiedHeaders });
---
+++ Locket.js
@@ -0,0 +1,43 @@
+const mapping = {
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
+};
+
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
+
+obj.Attention = "🎉 Chúc mừng bạn đã có huy hiệu Gold! Không chia sẻ công khai để tránh bị khóa!";
+
+var locket02 = {
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2099-12-18T01:04:17Z",
+  grace_period_expires_date: null,
+  unsubscribe_detected_at: null,
+  original_purchase_date: "2024-07-28T01:04:18Z",
+  purchase_date: "2024-07-28T01:04:17Z",
+  store: "app_store"
+};
+
+var dohungx = {
+  grace_period_expires_date: null,
+  purchase_date: "2024-07-28T01:04:17Z",
+  product_identifier: "com.locket02.premium.yearly",
+  expires_date: "2099-12-18T01:04:17Z"
+};
+
+const match = Object.keys(mapping).find(e => ua.includes(e));
+
+if (match) {
+  let [s] = mapping[match];
+  dohungx.product_identifier = s;
+  obj.subscriber.subscriptions[s] = locket02;
+  obj.subscriber.entitlements[match] = dohungx;
+} else {
+  obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+  obj.subscriber.entitlements.pro = dohungx;
+}
+
+$done({ body: JSON.stringify(obj) });
