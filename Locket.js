95943a93a4812a423dfcf45032e9756a94ced607
Nightmarket <162085003+NightmarketServer@users.noreply.github.com>
2025-08-15 19:35:45 +0700

    Update Locket.js

--- Locket.js
+++ Locket.js
@@ -1,64 +1,64 @@
-// Author: Locket Gold Unlock - Nightmarket
-// Bản đồ định danh User-Agent với entitlement và subscription tương ứng
+const version = 'V1.0.2';
+
+// Hàm chỉnh header
+function setHeaderValue(e, a, d) {
+    var r = a.toLowerCase();
+    r in e ? e[r] = d : e[a] = d;
+}
+
+// Xóa ETag để tránh cache
+var modifiedHeaders = $request.headers;
+setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+$done({ headers: modifiedHeaders });
+
+// Mapping UA sang gói VIP
 const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'], // App khác (có thể là app tiếng Trung)
-  'Locket': ['Gold'] // App Locket sẽ được gán quyền 'Gold'
+    '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+    'Locket': ['Gold']
 };
 
-// Lấy User-Agent từ header request
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-
-// Chuyển đổi nội dung phản hồi thành object
 var obj = JSON.parse($response.body);
 
-// Thêm thông điệp cảnh báo
+// Thông báo chúc mừng
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 
-// Thông tin gói đăng ký (subscription) giả lập
+// Thông tin gói mua giả
 var locket02 = {
-  is_sandbox: false,
-  ownership_type: "PURCHASED", // Đã mua
-  billing_issues_detected_at: null,
-  period_type: "normal",
-  expires_date: "2099-12-18T01:04:17Z", // Hết hạn rất xa
-  grace_period_expires_date: null,
-  unsubscribe_detected_at: null,
-  original_purchase_date: "2024-07-28T01:04:18Z",
-  purchase_date: "2024-07-28T01:04:17Z",
-  store: "app_store"
+    is_sandbox: false,
+    ownership_type: "PURCHASED",
+    billing_issues_detected_at: null,
+    period_type: "normal",
+    expires_date: "2099-12-18T01:04:17Z",
+    grace_period_expires_date: null,
+    unsubscribe_detected_at: null,
+    original_purchase_date: "2024-09-02T01:04:18Z",
+    purchase_date: "2024-09-02T01:04:17Z",
+    store: "app_store"
 };
 
-// Thông tin quyền lợi (entitlement) tương ứng với subscription
-var dohungx = {
-  grace_period_expires_date: null,
-  purchase_date: "2024-07-28T01:04:17Z",
-  product_identifier: "com.locket02.premium.yearly", // ID gói mặc định
-  expires_date: "2099-12-18T01:04:17Z"
+var locket01 = {
+    grace_period_expires_date: null,
+    purchase_date: "2024-09-02T01:04:17Z",
+    product_identifier: "com.locket02.premium.yearly",
+    expires_date: "2099-12-18T01:04:17Z"
 };
 
-// Kiểm tra xem User-Agent có khớp với app nào trong bảng mapping không
+// Tìm mapping theo UA
 const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
-  let [e, s] = mapping[match]; // Lấy tên entitlement và ID gói
-
-  if (s) {
-    // Nếu có chỉ định ID gói riêng
-    dohungx.product_identifier = s;
-    obj.subscriber.subscriptions[s] = locket02;
-  } else {
-    // Nếu không có ID riêng thì dùng ID mặc định
-    obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
-  }
-
-  // Gán entitlement theo tên app
-  obj.subscriber.entitlements[e] = dohungx;
+    let [e, s] = mapping[match];
+    if (s) {
+        locket01.product_identifier = s;
+        obj.subscriber.subscriptions[s] = locket02;
+    } else {
+        obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+    }
+    obj.subscriber.entitlements[e] = locket01;
 } else {
-  // Trường hợp không khớp User-Agent nào → gán mặc định
-  obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
-  obj.subscriber.entitlements.pro = dohungx;
+    obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+    obj.subscriber.entitlements.pro = locket01;
 }
 
-// Trả lại kết quả JSON sau khi chỉnh sửa
 $done({ body: JSON.stringify(obj) });
-
