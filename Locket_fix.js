0f460f83ac66e2c705f0790b8da10df81d9c946e
Nightmarket <162085003+NightmarketServer@users.noreply.github.com>
2025-10-03 17:03:44 +0700

    Update Locket_fix.js

--- Locket_fix.js
+++ Locket_fix.js
@@ -8,23 +8,10 @@ const mapping = {
 };
 
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-
-// Bắt lỗi parse
-var obj = {};
-try {
-  obj = JSON.parse($response.body);
-} catch (e) {
-  console.log("Parse error:", e);
-  $done({});
-}
-
-if (!obj.subscriber) obj.subscriber = {};
-if (!obj.subscriber.subscriptions) obj.subscriber.subscriptions = {};
-if (!obj.subscriber.entitlements) obj.subscriber.entitlements = {};
+var obj = JSON.parse($response.body);
 
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 
-// ========= Subscription ========= //
 var locket02 = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
@@ -33,25 +20,23 @@ var locket02 = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: specificDate, // thay bằng ngày mới
-  purchase_date: specificDate,         // thay bằng ngày mới
+  original_purchase_date: "2024-07-28T01:04:18Z",
+  purchase_date: "2024-07-28T01:04:17Z",
   store: "app_store"
 };
 
-// ========= Entitlement ========= //
 var dohungx = {
   grace_period_expires_date: null,
-  purchase_date: specificDate, // thay bằng ngày mới
+  purchase_date: "2024-07-28T01:04:17Z",
   product_identifier: "com.locket02.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
 
-// ========= Áp dụng mapping ========= //
 const match = Object.keys(mapping).find(e => ua.includes(e));
 
 if (match) {
   let [e, s] = mapping[match];
-
+  
   if (s) {
     dohungx.product_identifier = s;
     obj.subscriber.subscriptions[s] = locket02;
@@ -65,5 +50,4 @@ if (match) {
   obj.subscriber.entitlements.pro = dohungx;
 }
 
-// ========= Trả kết quả ========= //
 $done({ body: JSON.stringify(obj) });
