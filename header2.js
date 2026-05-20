af6e44272c5e1e9e6475ff67fe7d8e7f1cc4a978
Nightmarket <162085003+NightmarketServer@users.noreply.github.com>
2026-05-20 13:13:55 +0700

    Add new header2.js file

---
+++ header2.js
@@ -0,0 +1,14 @@
+/***********************************************
+> deleteHeader by NightMarket
+***********************************************/	
+/***********************************************
+ > Author: William - Thành Long
+***********************************************/
+const version = 'V1.0.2';
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}
+var modifiedHeaders=$request.headers;
+setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag","");
+$done({headers:modifiedHeaders});
+
+// ========= Nightmarket ========= //
+
