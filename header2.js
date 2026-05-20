93d4c58febb7f28521b79a36f1ae8b09edc70c17
Nightmarket <162085003+NightmarketServer@users.noreply.github.com>
2026-05-20 15:36:37 +0700

    Update header2.js

--- header2.js
+++ header2.js
@@ -1,14 +1,17 @@
-/***********************************************
-> deleteHeader by NightMarket
-***********************************************/	
-/***********************************************
- > Author: William - Thành Long
-***********************************************/
-const version = 'V1.0.2';
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}
-var modifiedHeaders=$request.headers;
-setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag","");
-$done({headers:modifiedHeaders});
+// deleteHeader.js - NightmarketServer
+// Remove headers that might be used by RevenueCat/app server to detect tampering
 
+var req = $request;
+var headers = req.headers || {};
+
+// Remove a set of headers that can trigger server-side checks
+delete headers['x-revenuecat-etag'];
+delete headers['x-revenuecat-signature'];
+delete headers['if-none-match'];
+delete headers['if-modified-since'];
+// Optionally remove user-agent if you want to force the UA from the config (uncomment if desired)
+// delete headers['user-agent'];
+
+$done({ headers: headers });
 // ========= Nightmarket ========= //
 
